51d1c4c4202628164e2cf409e40e09209b04bfd3
Matthew Timothy Erwin Penner <mtepenner@gmail.com>
2026-04-01 15:08:20 -0700

    Create mock_data.py

---
+++ backend/src/mock_data.py
@@ -0,0 +1,16 @@
+# The Feed Mill (Starting point for our delivery trucks)
+FEED_MILL = {
+    "id": "MILL-01",
+    "name": "Central Hub Mill",
+    "x_coord": 0.0,
+    "y_coord": 0.0
+}
+
+# A list of farms with silos monitored by sensors
+FARMS = [
+    {"id": "F-101", "name": "North Ridge Farm", "x_coord": 12.5, "y_coord": 45.2, "capacity_tons": 20.0, "current_tons": 4.5, "daily_burn_rate": 1.2},
+    {"id": "F-102", "name": "Valley Swine LLC", "x_coord": -22.1, "y_coord": 18.4, "capacity_tons": 30.0, "current_tons": 22.0, "daily_burn_rate": 2.5},
+    {"id": "F-103", "name": "Eastwood Poultry", "x_coord": 35.0, "y_coord": -10.5, "capacity_tons": 15.0, "current_tons": 2.1, "daily_burn_rate": 0.8},
+    {"id": "F-104", "name": "Oakhaven Dairy", "x_coord": -8.4, "y_coord": -30.2, "capacity_tons": 40.0, "current_tons": 8.0, "daily_burn_rate": 3.0},
+    {"id": "F-105", "name": "Pine Creek Feeders", "x_coord": 40.2, "y_coord": 25.6, "capacity_tons": 25.0, "current_tons": 18.0, "daily_burn_rate": 1.5},
+]
